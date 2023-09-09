0062b54baac4aeac74963bc0530a282949fda160
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-09-10 00:01:01 +0300

    feat: добавить crash-test

--- backend/app.js
+++ backend/app.js
@@ -66,6 +66,12 @@ app.use(cors({
 
 app.use(requestLogger); // подключаем логгер запросов
 
+// Проверит приложение на возобновление после ошибки
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post("/signin", validationSignin, login); // Роут логина
 app.post("/signup", validationSignup, createUser); // Роут регистрации
